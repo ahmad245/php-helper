81b2b94ac4804b3c23546a7a9d3d872cab3c03a6
ahmadalmasri <basimahgothman123@gmail.com>
2020-04-20 17:44:58 +0200

    TokenGenerator

---
+++ tokenGenerator.php
@@ -0,0 +1,18 @@
+<?php 
+
+class TokenGenerator
+{
+    private const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    public function getRandomSecureToken(int $length = 30): string
+    {
+        $token = '';
+        $maxNumber = strlen(self::ALPHABET);
+
+        for ($i = 0; $i < $length; $i++) {
+            $token .= self::ALPHABET[random_int(0, $maxNumber - 1)];
+        }
+
+        return $token;
+    }
+}
