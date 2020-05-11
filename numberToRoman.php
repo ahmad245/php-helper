4aba4565b1a1b6cc98cf5d956401509b699515ce
Ahmad AL MASRI <a.almasri@onlineformapro.org>
2020-05-11 15:03:44 +0200

    numberToRoman.php

---
+++ numberToRoman.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Converts a number to its roman presentation.
+ **/ 
+function numberToRoman($num)  
+{ 
+    // Be sure to convert the given parameter into an integer
+    $n = intval($num);
+    $result = ''; 
+ 
+    // Declare a lookup array that we will use to traverse the number: 
+    $lookup = array(
+        'M' => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400, 
+        'C' => 100, 'XC' => 90, 'L' => 50, 'XL' => 40, 
+        'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1
+    ); 
+ 
+    foreach ($lookup as $roman => $value)  
+    {
+        // Look for number of matches
+        $matches = intval($n / $value); 
+ 
+        // Concatenate characters
+        $result .= str_repeat($roman, $matches); 
+ 
+        // Substract that from the number 
+        $n = $n % $value; 
+    } 
+
+    return $result; 
+} 
+
+
+// VIII
+echo numberToRoman(8);
+
+// CXXIII
+echo numberToRoman(123);
+
+// MMCCCLV
+echo numberToRoman(2355);
+
+// MMMMCMXCIX
+echo numberToRoman(4999);
